96abfa30c2202ee584ed97648319f1d827ffd3b2
minheekangg <minheekangg@gmail.com>
2020-07-08 17:32:46 -0400

    new

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const App =  () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Route path="/" exact component={} />
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default App;
