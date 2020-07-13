7e1a11b9d22e9a7b94c40b4ad446b0076a9c705a
minheekangg <minheekangg@gmail.com>
2020-07-13 16:25:47 -0400

    added sign in and sign out using gapi

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -25,13 +25,31 @@ export default class GoogleAuth extends React.Component {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
     }
 
+    onSignIn = () => {
+        this.auth.signIn();
+    }
+
+    onSignOut = () => {
+        this.auth.signOut();
+    }
+
     renderAuthButton(){
         if (this.state.isSignedIn === null) {
-            return <div>I dont know</div>
+            return null;
         } else if (this.state.isSignedIn) {
-            return <div>I am signed in</div>
+            return (
+                <button className="ui red google button" onClick={this.onSignOut}>
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            )
         } else {
-            return <div>I am signed out</div>
+            return (
+                <button className="ui red google button" onClick={this.onSignIn}>
+                    <i className="google icon" />
+                    Sign In
+                </button>
+            )
         }
     }
 
