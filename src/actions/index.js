4d2882f2b98bf7da4383e753bdbdf4c7c50dba4e
minheekangg <minheekangg@gmail.com>
2020-07-16 15:59:11 -0400

    restful actions all set up

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,6 +14,27 @@ export const signOut = () => {
     }
 }
 
-export const createStream = payload => async dispatch => {
-    streams.post('/streams', payload)
+export const createStream = data => async dispatch => {
+    const response = await streams.post('/streams', data);
+    dispatch({ action: ActionTypes.CREATE_STREAM, payload: response.data})
+}
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({ action: ActionTypes.FETCH_STREAMS, payload: response.data})
+}
+
+export const fetchStream = id => async dispatch => {
+    const response = await streams.get('/streams/' + id);
+    dispatch({ action: ActionTypes.FETCH_STREAM, payload: response.data})
+}
+
+export const editStream = (id, data) => async dispatch => {
+    const response = await streams.put('/streams/' + id, data);
+    dispatch({ action: ActionTypes.EDIT_STREAM, payload: response.data})
+}
+
+export const deleteStream = id => async dispatch => {
+    await streams.delete('/streams' + id);
+    dispatch({ action: ActionTypes.DELETE_STREAM, payload: id})
 }
