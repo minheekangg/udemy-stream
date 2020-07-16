3245e8baf46d9c2e855f653bf26ba17a73efd6b7
minheekangg <minheekangg@gmail.com>
2020-07-16 16:33:17 -0400

    show stream shows all streams

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -5,12 +5,40 @@ import { fetchStreams } from '../../actions/index';
 
 class StreamList extends React.Component {
     componentDidMount() {
-        console.log('props', this.props)
         this.props.fetchStreams()
     }
+
+    renderList() {
+        return this.props.streams.map(stream=> {
+            return (
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description"> {stream.description} </div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
     render() {
-        return <div>hiStreamList</div>
+        return( 
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">
+                    {this.renderList()} 
+                </div>
+            </div>
+        )
     }
 }
 
-export default connect(null, { fetchStreams })(StreamList);
+
+const mapStateToProps = state => {
+    return {
+        streams: Object.values(state.stream)
+    }
+}
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
