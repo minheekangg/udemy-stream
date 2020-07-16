2722719ccc07e8de3e6a94310017a620f57cfb96
minheekangg <minheekangg@gmail.com>
2020-07-16 16:12:57 -0400

    added delete

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,11 +1,14 @@
 import { ActionTypes } from '../actions/types';
+import _ from 'lodash';
 
 export default (state={}, action) => {
     switch (action.type) {
         case ActionTypes.FETCH_STREAM: 
         case ActionTypes.CREATE_STREAM:
         case ActionTypes.EDIT_STREAM:
-            return { ...state, [action.payload.id]: action.payload }
+            return { ...state, [action.payload.id]: action.payload };
+        case ActionTypes.DELETE_STREAM:
+            return _.omit(state, action.payload);
         default: 
             return state;
     }
